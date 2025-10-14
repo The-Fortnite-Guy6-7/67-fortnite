7ae2d1d32e73b4bd64e75f88a7ae0714e7108853
The-Fortnite-Guy6-7 <devingokublackballz42907@gmail.com>
2025-10-14 08:55:02 -0700

    started the project.

---
+++ Assets/HazardController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class HazardController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Update()
+    {
+       public float speed = 5
+
+        
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    Vector3 pos = transform.position
+        pos.x += 5 * Time.deltaTime;
+    transform.position = pos;
+
+    }
+}
